8d82f51f8b4c8adc9ee9e34039ac0dd540d67a68
ctysingh <chaitanyasingh@gmail.com>
2023-08-13 07:10:26 -0400

    add base component for todolist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {hot} from "react-hot-loader";
+import Todolist from "./Todos/Todolist";
 import './App.css'
 
 const App = ()=> (
     <div className="App">
-        <h1>Hello, World!!</h1>
+       <Todolist></Todolist>
     </div>
+    
 )
 
 export default hot(module) (App);
---
+++ src/Todos/Todolist.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Todolist.css"
+
+const Todolist = () => (
+    <div>
+        <h1>To Do list componnet</h1>
+    </div>
+)
+
+export default Todolist;

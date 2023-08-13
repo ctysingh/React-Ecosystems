14cc125d3a77b58b2db3235f4daeafaa1eb7e0db
ctysingh <chaitanyasingh@gmail.com>
2023-08-13 18:16:43 -0400

    wrapping appp component with provide from redux stor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import {configureStore} from './store'
 import  ReactDOM  from 'react-dom';
 import App from './App.js'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={configureStore()}>
+         <App/>
+    </Provider>, 
+    document.getElementById('root')
+    );

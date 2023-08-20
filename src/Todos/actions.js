27a7a8e1bb5a30159e94ab27cb50f248f6784525
ctysingh <chaitanyasingh@gmail.com>
2023-08-20 11:31:45 -0400

    fixed the payload to a json object

--- src/Todos/actions.js
+++ src/Todos/actions.js
@@ -2,12 +2,12 @@ export const CREATE_TODO = "CREATE_TODO"
 // action creator
 export const createTodo = text => ({
     type:CREATE_TODO,
-    payload:text
+    payload:{text}
 });
 
 export const REMOVE_TODO = 'REMOVE_TODO'
 // action creator
 export const removeTodo = text => ({
     type:REMOVE_TODO,
-    payload:text
+    payload:{text}
 });

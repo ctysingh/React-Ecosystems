ae8ffe33c7b5a8d3bd2d8592d0b3b41605c2a996
ctysingh <chaitanyasingh@gmail.com>
2023-09-04 05:57:50 -0400

    added loadtodos to thunk

--- src/Todos/actions.js
+++ src/Todos/actions.js
@@ -1,19 +1,36 @@
-export const CREATE_TODO = "CREATE_TODO"
+export const CREATE_TODO = "CREATE_TODO";
 // action creator
 export const createTodo = text => ({
     type:CREATE_TODO,
     payload:{text}
 });
 
-export const REMOVE_TODO = 'REMOVE_TODO'
+export const REMOVE_TODO = 'REMOVE_TODO';
 // action creator
 export const removeTodo = text => ({
     type:REMOVE_TODO,
     payload:{text}
 });
 
-export const MARK_AS_COMPLETED = 'MARK_AS_COMPLETED'
+export const MARK_AS_COMPLETED = 'MARK_AS_COMPLETED';
 export const markAsCompleted = text => ({
     type:MARK_AS_COMPLETED,
     payload:{text}
 })
+
+export const LOAD_TO_DOS_PROGRESS = 'LOAD_TO_DOS_PROGRESS';
+export const loadToDosProgress = () => ({
+    type:LOAD_TO_DOS_PROGRESS,
+});
+
+export const LOAD_TO_DOS_SUCCESS = 'LOAD_TOD_DOS_SUCCESS';
+export const loadToDosSuccess = (todos) => ({
+    type: LOAD_TO_DOS_SUCCESS,
+    payload:{todos},
+});
+
+export const LOAD_TOD_DOS_FAILURE = 'LOAD_TO_DODS_FAILURE';
+export const loadToDosFailure = () => ({
+    type:LOAD_TOD_DOS_FAILURE,
+    
+});
---
+++ src/thunks.js
@@ -0,0 +1,18 @@
+import { loadToDosProgress, loadToDosSuccess, loadToDosFailure } from "./Todos/actions";
+
+export const loadToDos = () => async (dispatch, getState) => {
+    try {
+        dispatch(loadToDosProgress);
+        const response = await fetch('http://localhost:8080/todos');
+        const todos = await response.json;
+        dispatch(loadToDosSuccess(todos));
+    } catch(e){
+        dispatch(loadToDosFailure);
+        dispatch(displayAlert(e));
+    }   
+
+}
+
+export const displayAlert = (text) => () => {
+    alert(text);
+};
